83d89d6f77f9ae6185ac5beb39df8bbbdbc77a89
broadsword007 <ferhan0000@gmail.com>
2022-09-10 15:47:33 -0700

    Setup react router, landing page, about page and a page for each team member, some minor code refactoring, replaced some default app-names/descriptions with HiringGuru specific stuff

---
+++ application/frontend/hiring_guru_fe/src/constants.js
@@ -0,0 +1,14 @@
+import Member from "./dataclasses/Member";
+
+const MEMBERS = [
+    new Member(
+        "Farhan Haider",
+        "shaider1@sfsu.edu",
+        `Hi, my name is Farhan and I am from Lahore, Pakistan. I've been 
+        working in the software industry for the last 4 years. I am really 
+        excited to resume academic learning. Since I just moved to SF, I am 
+        still trying to adjust to the new culture and dynamics.`
+    )
+]
+
+export {MEMBERS}
