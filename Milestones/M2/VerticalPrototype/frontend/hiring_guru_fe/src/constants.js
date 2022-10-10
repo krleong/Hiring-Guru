747d94ec5ef647e911cbd32f7d1935e141398c52
krleong <krleong1234@gmail.com>
2022-10-09 22:35:26 -0700

    Installed Bootstrap

---
+++ Milestones/M2/VerticalPrototype/frontend/hiring_guru_fe/src/constants.js
@@ -0,0 +1,43 @@
+import Member from "./dataclasses/Member";
+
+const MEMBERS = [
+    new Member(
+        "Farhan Haider",
+        "shaider1@sfsu.edu",
+        `Hi, my name is Farhan and I am from Lahore, Pakistan. I've been 
+        working in the software industry for the last 4 years. I am really 
+        excited to resume academic learning. Since I just moved to SF, I am 
+        still trying to adjust to the new culture and dynamics.`
+    ),
+    new Member(
+        "Kenny Leong",
+        "kleong2@mail.sfsu.edu",
+        `Hi, I'm Kenny, and I'm an incoming senior in Computer Science with a
+        minor in Math. I'm originally from SoCal and am attending SF State because
+        I love the area, and the abundance of opportunities in tech here is a huge
+        plus. In my free time, I enjoy taking care of my pets, which include 3 dogs
+        and 4 chickens.`
+    ),
+    new Member(
+        "Khushi Khanna",
+        "kkhanna@sfsu.edu",
+        `Hi, I'm Khushi! I'm originally from the Bay Area, and I am a senior majoring
+         in computer science. I am excited to gain more experience working in the
+         backend, since I have minimal experience with software engineering.`
+   ),
+    new Member(
+        "Mamadou Bah",
+        "mbah1@mail.sfsu.edu",
+        `Hey! My name is Mamadou and I am a fourth year student at San Fransisco State
+        University studying Computer Science. I am originally from the Bay Area and 
+        am very excited to wrap up my final semesters with this school and gain as
+        much experience as I can with software engineering!`
+   ),
+        new Member(
+        "Eric Leow",
+        "eleow@mail.sfsu.edu",
+        `Hi my name is Eric and I started college with a focus on game development and then quickly switched to compter science when I started going to SF state`
+   ),
+]
+
+export { MEMBERS }
