f6c2cfa577fb0f0d7254138cb8abab1ac9eae0ff
broadsword007 <ferhan0000@gmail.com>
2022-11-08 11:30:13 -0800

    added page for setting up recruitment process, added a reusable global dialog

--- Milestones/M3/HorizontalPrototype/frontend/hiring_guru_fe/src/HiringGuru.js
+++ Milestones/M3/HorizontalPrototype/frontend/hiring_guru_fe/src/HiringGuru.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './HiringGuru.css';
 import About from './pages/About/About';
@@ -15,34 +15,89 @@ import NavBar from './components/NavBar/NavBar.js';
 import Footer from './components/Footer/Footer.js';
 
 import Tagline from './components/Tagline/Tagline.js';
+import RecruitmentProcess from "./pages/RecruitmentProcess/RecruitmentProcess";
+import {Dialog} from "./components/Dialog/Dialog";
+
+export const ApplicationContext = React.createContext();
 
 function HiringGuru() {
+  const [appState, setAppState] = useState({
+    dialogState: {
+      title: "",
+      dialogOpen: false,
+      actions: [],
+      body: ""
+    }
+  })
+
   useEffect(() => {
     document.title = "Hiring Guru";
   }, []);
+
+  const openDialog = (title, actions = [], body="") => {
+    setAppState({
+      ...appState,
+      dialogState: {
+        ...appState.dialogState,
+        dialogOpen: true,
+        actions: actions,
+        title: title,
+        body: body
+      }
+    })
+  }
+
+  const closeDialog = () => {
+    setAppState({
+      ...appState,
+      dialogState: {
+        ...appState.dialogState,
+        dialogOpen: false
+      }
+    })
+  }
+
   return (
     <Router>
-      <div>
-        <div className="page-wrapper">
-          <div className="sticky-top-follow">
-            <Tagline></Tagline>
-            <NavBar></NavBar>
+      <Dialog
+          show={appState.dialogState.dialogOpen}
+          title={appState.dialogState.title}
+          body={appState.dialogState.body}
+          actions={appState.dialogState.actions.length===0 ? [
+            {
+              title: "Close",
+              handler: closeDialog,
+              variant: "secondary"
+            }
+          ]: appState.dialogState.actions}
+      ></Dialog>
+      <ApplicationContext.Provider value={{
+        openDialog: openDialog,
+        closeDialog: closeDialog
+      }}>
+        <div>
+          <div className="page-wrapper">
+            <div className="sticky-top-follow">
+              <Tagline></Tagline>
+              <NavBar></NavBar>
+            </div>
+            <Routes>
+              <Route path='/' element={<Landing />} />
+              <Route path='/jobs' element={<JobPosts />} />
+              <Route path='/pricing' element={<Pricing />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<LogIn />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/create-company' element={<CreateCompany />} />
+              <Route path='/free-trial' element={<FreeTrial/>} />
+              <Route path='/create-job' element={<CreateJob/>} />
+              <Route path='/recruitment-process' element={<RecruitmentProcess />} />
+              <Route path='/about/members/:index/detail' element={<Member />} />
+            </Routes>
           </div>
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/jobs' element={<JobPosts />} />
-            <Route path='/pricing' element={<Pricing />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<LogIn />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/create-company' element={<CreateCompany />} />
-            <Route path='/free-trial' element={<FreeTrial/>} />
-            <Route path='/create-job' element={<CreateJob/>} />
-            <Route path='/about/members/:index/detail' element={<Member />} />
-          </Routes>
+            <Footer></Footer>
         </div>
-          <Footer></Footer>
-      </div>
+      </ApplicationContext.Provider>
     </Router>
   );
 }
