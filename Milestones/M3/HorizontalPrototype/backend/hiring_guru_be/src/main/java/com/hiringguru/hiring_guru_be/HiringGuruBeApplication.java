e48ad9ea063aba7b04d215fbb46a852656a8eaca
broadsword007 <ferhan0000@gmail.com>
2022-11-10 10:54:21 -0800

    SSL verify

--- Milestones/M3/HorizontalPrototype/backend/hiring_guru_be/src/main/java/com/hiringguru/hiring_guru_be/HiringGuruBeApplication.java
+++ Milestones/M3/HorizontalPrototype/backend/hiring_guru_be/src/main/java/com/hiringguru/hiring_guru_be/HiringGuruBeApplication.java
@@ -15,6 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -58,6 +61,16 @@ public class HiringGuruBeApplication {
     return "Welcome to HiringGuru";
   }
 
+  @RequestMapping({"/.well-known/pki-validation/4445F6A0F2E475894038C9255E286218.txt"})
+  public ResponseEntity<byte[]> sslVerification() {
+    String demoContent = "B3D7A9BC6BB99400115AA22B7FB4675EE364C68A24C83D6702556BDACB61A7EF\n" +
+            "comodoca.com\n" +
+            "ac3a2f187ffff7a"; // (2) Dynamic content
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // (3) Content-Type: application/octet-stream
+    httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename("4445F6A0F2E475894038C9255E286218.txt").build().toString()); // (4) Content-Disposition: attachment; filename="demo-file.txt"
+    return ResponseEntity.ok().headers(httpHeaders).body(demoContent.getBytes()); // (5) Return Response
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(HiringGuruBeApplication.class, args);
